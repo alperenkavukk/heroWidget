ae56a8ec03455d3ef5858da3a27dd03c47abfcd9
alperenkavukk <kavuk.67.76@gmail.com>
2022-09-02 13:54:19 +0300

    Initial Commit

---
+++ HeroWidget/HeroWidgetApp.swift
@@ -0,0 +1,17 @@
+//
+//  HeroWidgetApp.swift
+//  HeroWidget
+//
+//  Created by Alperen Kavuk on 2.09.2022.
+//
+
+import SwiftUI
+
+@main
+struct HeroWidgetApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ HeroWidgetUITests/HeroWidgetUITestsLaunchTests.swift
@@ -0,0 +1,32 @@
+//
+//  HeroWidgetUITestsLaunchTests.swift
+//  HeroWidgetUITests
+//
+//  Created by Alperen Kavuk on 2.09.2022.
+//
+
+import XCTest
+
+class HeroWidgetUITestsLaunchTests: XCTestCase {
+
+    override class var runsForEachTargetApplicationUIConfiguration: Bool {
+        true
+    }
+
+    override func setUpWithError() throws {
+        continueAfterFailure = false
+    }
+
+    func testLaunch() throws {
+        let app = XCUIApplication()
+        app.launch()
+
+        // Insert steps here to perform after app launch but before taking a screenshot,
+        // such as logging into a test account or navigating somewhere in the app
+
+        let attachment = XCTAttachment(screenshot: app.screenshot())
+        attachment.name = "Launch Screen"
+        attachment.lifetime = .keepAlways
+        add(attachment)
+    }
+}
